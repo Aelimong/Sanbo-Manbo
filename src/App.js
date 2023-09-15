2b8e4d2894d60b66fc4d808fbda2091c34bb8b5c
Aelimong <dofla0507@gmail.com>
2023-09-15 12:03:48 +0900

    feat(AeLim): 메인 페이지 트레킹 추천 섹션 레이아웃

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Header from "./components/Header";
 import "./styles/style.scss";
+import Header from "./components/Header";
+import ThemaTreck from "./components/ThemaTreck";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         {/* 2. 내 주변 위치 */}
         {/* 3. 띠 배너 */}
         {/* 4. 주제별 트레킹 */}
+        <ThemaTreck />
         {/* 5. 큐레이션 지도 */}
         {/* 6. 크루모집 */}
         {/* 7. 산보만보 스토리 */}
---
+++ src/components/ThemaTreck.jsx
@@ -0,0 +1,51 @@
+import "../styles/mainAeLim.scss";
+import levelThema from "../assets/img/mainAeLim/level_thema_1.jpg";
+import seosonThema from "../assets/img/mainAeLim/seoson_thema.jpg";
+import { Link } from "react-router-dom";
+
+export default function ThemaTreck() {
+  return (
+    <>
+      <section className="sec03 wrapper">
+        <div className="thema level-thema">
+          <div className="text-area">
+            <span>내가 가고 싶은 곳은?</span>
+            <h3>난이도별 트레킹</h3>
+            <p>초급, 중급, 상급까지 다양하게</p>
+            <button>
+              <Link to="/test1">
+                바로가기&nbsp;&nbsp;
+                <i
+                  className="fa-solid fa-arrow-right"
+                  style={{ color: "#050505" }}
+                ></i>
+              </Link>
+            </button>
+          </div>
+          <figure className="img-area">
+            <img src={levelThema} alt="난이도별 트레킹" />
+          </figure>
+        </div>
+        <div className="thema seoson-thema">
+          <div className="text-area">
+            <span>내가 가고 싶은 곳은?</span>
+            <h3>계절별 트레킹</h3>
+            <p>산에서 계절을 느껴보세요</p>
+            <button>
+              <Link to="/test2">
+                바로가기&nbsp;&nbsp;
+                <i
+                  className="fa-solid fa-arrow-right"
+                  style={{ color: "#050505" }}
+                ></i>
+              </Link>
+            </button>
+          </div>
+          <figure className="img-area">
+            <img src={seosonThema} alt="계절별 트레킹" />
+          </figure>
+        </div>
+      </section>
+    </>
+  );
+}
